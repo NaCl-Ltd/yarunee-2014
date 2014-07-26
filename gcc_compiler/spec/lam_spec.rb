598d57f8d6193f1487cdf5e026e0d598b015240b
Yutaka HARA <yutaka.hara+github@gmail.com>
2014-07-26 20:30:34 +0900

    テストを追加

--- gcc_compiler/spec/lam_spec.rb
+++ gcc_compiler/spec/lam_spec.rb
@@ -12,8 +12,43 @@ LDC 1
 LDC 2
 ADD
 RTN
+--
+(cons 1 2)
+:
+LDC 1
+LDC 2
+CONS
+RTN
+--
+(if 7 8 9)
+:
+LDC 7
+SEL 3 5
+RTN
+LDC 8
+JOIN
+LDC 9
+JOIN
+--
+((lambda (x) x) 7)
+:
+LDC 7
+LDF 4
+AP 1
+RTN
+LD 0 0
+RTN
+--
+(+ 7 (inline "DBG\nSTOP"))
+:
+LDC 7
+DBG
+STOP
+ADD
+RTN
 EOD
-TESTS = TESTS_.split(/^--$/).map{|x|
+
+TESTS = TESTS_.split(/^--.*$/).map{|x|
   scm, gcc = *x.split(/^:$/)
   [scm.strip, gcc.lstrip]
 }
@@ -21,7 +56,7 @@ TESTS = TESTS_.split(/^--$/).map{|x|
 module Lam
   describe 'Lam' do
     TESTS.each do |scm, gcc|
-      it "should compile #{scm}" do
+      it "should compile #{scm.inspect}" do
         expect(Lam.compile(scm)).to eq(gcc)
       end
     end
